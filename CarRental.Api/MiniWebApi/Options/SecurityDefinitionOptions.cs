e2df188ad6a3d1b92840607320a0372781ff4119
Tatsiana Kozlyakovskaya <kozlyakovskaya_tatsiana@mail.ru>
2020-10-12 22:35:45 +0300

    add configuring swagger in swaggeroptions.json

---
+++ CarRental.Api/MiniWebApi/Options/SecurityDefinitionOptions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniWebApi.Options
+{
+    public class SecurityDefinitionOptions
+    {
+        public const string SectionName = "SecurityDefinitionOptions";
+
+        public string SecuriyDefinitionName { get; set; }
+
+        public string Description { get; set; }
+
+        public string Name { get; set; }
+
+        public string Scheme { get; set; }
+    }
+}
---
+++ CarRental.Api/MiniWebApi/Services/TokenService.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using MiniWebApi.JWT;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniWebApi.Services
+{
+    public class TokenService
+    {
+        public readonly JwtOptions JwtOptions;
+
+        public TokenService(IOptions<JwtOptions> options)
+        {
+            JwtOptions = options.Value;
+        }
+
+        public string GenerateToken(IEnumerable<Claim> claims, int lifeTime)
+        {
+            var jwt = new JwtSecurityToken(
+                    issuer: JwtOptions.Issuer,
+                    audience: JwtOptions.Audience,
+                    claims: claims,
+                    expires: DateTime.UtcNow.AddMinutes(lifeTime),
+                    signingCredentials: new SigningCredentials(JwtOptions.SymmetricSecurityKey, SecurityAlgorithms.HmacSha256));
+
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            return encodedJwt;
+        }
+
+        public ClaimsPrincipal ValidateToken(string token)
+        {
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = false, 
+                ValidateIssuer = false,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = JwtOptions.SymmetricSecurityKey,
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+         
+            var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out SecurityToken securityToken);
+
+            return principal;
+        }
+    }
+}
