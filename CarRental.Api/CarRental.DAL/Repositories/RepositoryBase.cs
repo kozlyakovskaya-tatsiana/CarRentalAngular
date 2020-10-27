2bde2da284ecfa05879d9bceb2233a43a462359b
ICX\T.Kazliakouskaya <T.Kazliakouskaya@itechart-group.com>
2020-10-27 16:55:59 +0300

    make repository async

--- CarRental.Api/CarRental.DAL/Repositories/RepositoryBase.cs
+++ CarRental.Api/CarRental.DAL/Repositories/RepositoryBase.cs
@@ -1,72 +1,73 @@
-﻿using System;
+﻿using CarRental.DAL.EFCore;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using CarRental.DAL.EFCore;
-using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace CarRental.DAL.Repositories
 {
     public class EFGenericRepository<TEntity> : IRepository<TEntity> where TEntity : class, IEntity
     {
-        private readonly ApplicationIdentityContext _context;
+        private readonly ApplicationContext _context;
 
         private readonly DbSet<TEntity> _dbSet;
 
-        public EFGenericRepository(ApplicationIdentityContext context)
+        public EFGenericRepository(ApplicationContext context)
         {
             _context = context;
 
             _dbSet = context.Set<TEntity>();
         }
-        public void Create(TEntity entity)
+        public async Task CreateAsync(TEntity entity)
         {
             if (entity != null)
             {
-                _dbSet.Add(entity);
+                await _dbSet.AddAsync(entity);
             }
         }
 
-        public TEntity FindById(int id)
+        public async Task<TEntity> FindByIdAsync(int id)
         {
-            return _dbSet.Find(id);
+            return await _dbSet.FindAsync(id);
         }
 
-        public IEnumerable<TEntity> Get()
+        public async Task<IEnumerable<TEntity>> GetAsync()
         {
-            return _dbSet.ToArray();
+            return await _dbSet.ToArrayAsync();
         }
 
-        public IEnumerable<TEntity> Get(Func<TEntity, bool> predicate)
+        public async Task<IEnumerable<TEntity>> Get(Func<TEntity, bool> predicate)
         {
-            return _dbSet.Where(predicate).ToList();
+            return await Task.Run( () => _dbSet.Where(predicate).ToArray());
         }
 
-        public void Remove(TEntity entity)
+        public async Task Remove(TEntity entity)
         {
             if (entity != null)
-                _dbSet.Remove(entity);
+               await Task.Run(() =>  _dbSet.Remove(entity));
         }
 
-        public void Remove(int id)
+        public async Task Remove(int id)
         {
-            var entityToDelete = _dbSet.FirstOrDefault(el => el.Id == id);
+            var entityToDelete = await _dbSet.FirstOrDefaultAsync(el => el.Id == id);
 
             if (entityToDelete != null)
             {
-                _dbSet.Remove(entityToDelete);
+                await Task.Run( () => _dbSet.Remove(entityToDelete));
             }
         }
 
-        public void SaveChanges()
+        public async Task SaveChanges()
         {
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
         }
 
-        public void Update(TEntity entity)
+        public async Task Update(TEntity entity)
         {
             if (entity != null)
             {
-                _dbSet.Update(entity);
+                await Task.Run( () => _dbSet.Update(entity));
             }
         }
     }
